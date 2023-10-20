fdf3a3d1281cec190e36fe6d4b29e8e217ddba38
Shiya Shalmali Shetty <137711284+shiyashalmali@users.noreply.github.com>
2023-10-20 20:35:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+// prompt("enter your name","");
+// console.log("this is  console");
+// console.log(6+5);
+// // document.write("<h1>js</h1>");
+
+// var num1=5;
+// var num1=6; //we can redeclare the variable
+// num1=8  //we can reassign the variable
+// console.log(num1);
+
+// let num2=9
+// let num2=10 //we cannot redeclare the variable
+// num2=11  //we can reassign the variable
+// console.log(num2);
+
+// const num3=10
+// const num3=11 //we cannot redeclare the variable
+// num3=15 ////we cant reassign the variable
+// console.log(num3);
+
+// console.log(3**4);
+// let x=5
+// x++
+// console.log(x);
+// x--
+// console.log(x);
+
+// let num=5
+// num*=2
+// console.log(num)
+
+// let num1=5
+// let num2="5"
+// console.log(num1==num2);
+// console.log(num1===num2);
+
+// function mul(n1,n2,n3,n4)
+// {
+//     return n1*n2*n3*n4;
+// }
+// document.write(mul(4,5,6,7))
+// function greet(){
+//     alert("hello everyone");
+// }
+
+// let age=10
+// if(age>18){
+//     document.write("eligible to vote");
+// }
+// else{
+//     document.write("not eligible to vote");
+// }
+
+// for(let num=10;num>=1;num--){
+//     console.log(num);
+// }
+
+// Dom-Document Object Module
+// Dom Manipulation Methods:
+// document.getElementById()
+// document.getElementsByClassName()
+// document.getElementsByTagName()
+// document.querySelector()
+// document.querySelectorAll()
+
+// let el=document.getElementById("head").innerHTML;
+// console.log(el);
+
+let ele=document.querySelector("#head").innerHTML
+console.log(ele);
+
